d62f37cec087a3cdc696b476c0ed6d1ff3933891
Bailey <bailey_fun@live.com.au>
2020-01-23 11:03:54 +1100

    Ausopen

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import csv
 import math
+import os.path
 import pandas as pd
+import re
 import time
 
 
@@ -9,6 +11,7 @@ games = {}
 wins = []
 accuracies = []
 last_match = {}
+player_list = {}
 
 P = 0
 
@@ -47,6 +50,10 @@ def analyse(matches: pd.DataFrame) -> None:
     best(matches)
     accuracy()
 
+    global player_list
+    player_list = {x: get_player(matches, x) for x in elo}
+    player_list = {k: str(v) for k, v in sorted(player_list.items(), key=lambda x: last_match[x[0]], reverse=True)}
+
 
 def analyse_matches() -> None:
     hist = pd.read_excel("results.xlsx")
@@ -78,6 +85,26 @@ def best(matches: pd.DataFrame) -> None:
         print(f"{get_player(matches, player[0])}: {player[1]}")
 
 
+def do_aus_open(matches: pd.DataFrame, aus_open: pd.DataFrame) -> None:
+    open_player_list = {}
+    for i, game in aus_open.iterrows():
+        # print(game)
+        for player in [game[1], game[2]]:
+            if player not in open_player_list.keys() and player not in ['-']:
+                open_player_list[player] = search_by_name_parts(player[0], player[3:])
+
+        if "-" in [game[1], game[2]]:
+            continue
+
+        p1 = open_player_list[game[1]]
+        p2 = open_player_list[game[2]]
+
+        print(game["Me"])
+
+        print([game[1], p1, game[2], p2, predict(p1, p2)])
+    pass
+
+
 def get_player(matches: pd.DataFrame, player_id: int) -> str:
     try:
         player = matches.loc[matches['winner_id'] == player_id]['winner_name'].unique()[0]
@@ -86,6 +113,7 @@ def get_player(matches: pd.DataFrame, player_id: int) -> str:
             player = matches.loc[matches['loser_id'] == player_id]['loser_name'].unique()[0]
         except IndexError:
             print(["Player not found", player_id])
+            player = ""
     return player
 
 
@@ -94,27 +122,38 @@ def load() -> pd.DataFrame:
     for year in range(1968, 2021):
         try:
             if year == 2020:
-                data = data.append(pd.read_csv(f"tennis_atp/atp_matches_{year}.csv").rename(columns={"loser_id":"fake_loser_id","winner_rank":"loser_id"}), sort=False)
+                data = data.append(pd.read_csv(f"tennis_atp/atp_matches_{year}.csv")
+                                   .rename(columns={"loser_id": "fake_loser_id", "winner_rank": "loser_id"}), sort=False)
             else:
                 data = data.append(pd.read_csv(f"tennis_atp/atp_matches_{year}.csv"), sort=False)
             print(f"Loading data from {year}...")
         except FileNotFoundError:
             print(f"No data found for {year}.")
+    # data.to_csv("all_matches.csv")
+
     return data
+
+
+def load_aus_open() -> pd.DataFrame:
+    aus_open = pd.read_excel("results.xlsx", nrows=127)
+    return aus_open
 
 
 def main() -> None:
     matches = load()
     analyse(matches)
-    verse(matches)
-    # search(matches)
+    # verse(matches)
+    #
     # predict_players(matches)
+    aus_open2020 = load_aus_open()
+    do_aus_open(matches, aus_open2020)
+    search(matches)
     # analyse_matches()
 
 
 def predict(player1: int, player2: int) -> float:
-    s1 = elo[player1]
-    s2 = elo[player2]
+    s1 = elo[player1] if player1 != 0 else 1500
+    s2 = elo[player2] if player2 != 0 else 1500
     p = 1.0 / (1.0 + math.pow(10, ((s2 - s1) / 400)))
     return p
 
@@ -135,8 +174,6 @@ def predict_players(matches: pd.DataFrame):
 
 
 def search(matches: pd.DataFrame) -> None:
-    player_list = {x: get_player(matches, x) for x in elo}
-    player_list = {k: v for k, v in sorted(player_list.items(), key=lambda x: last_match[x[0]], reverse=True)}
     while True:
         query = input("Name:")
         search_results = [[x, player_list[x], last_match[x]]
@@ -149,7 +186,16 @@ def search(matches: pd.DataFrame) -> None:
             print("No player found.")
 
 
-def search_players(prompt: str, player_list: dict) -> [str, str]:
+def search_by_name_parts(initial: str, surname: str) -> [str, str]:
+    search_names = re.findall(r"[\w]+", surname)
+    search_results = [x
+                      for x in player_list
+                      if not all(s == -1 for s in [player_list[x].lower().find(f.lower()) for f in search_names])
+                      and player_list[x].lower()[0] == initial.lower()]
+    return search_results[0] if len(search_results) > 0 else 0
+
+
+def search_players(prompt: str) -> [str, str]:
     query = input(prompt)
     search_results = [[x, player_list[x], last_match[x]]
                       for x in player_list
@@ -160,25 +206,24 @@ def search_players(prompt: str, player_list: dict) -> [str, str]:
         player = [search_results[0][0], search_results[0][1]]
     else:
         print("No player found.")
-        player = search_players(prompt, player_list)
+        player = search_players(prompt)
     return player
 
 
 def update(player: int, expected: float, actual: int) -> None:
+    if player == 0:
+        return
     c = 250
     o = 5
     s = 0.4
     k = c / math.pow((games[player] + o), s)
     elo[player] = elo[player] + k * (actual - expected)
-    pass
 
 
-def verse(matches: pd.DataFrame) -> None:
-    player_list = {x: get_player(matches, x) for x in elo}
-    player_list = {k: str(v) for k, v in sorted(player_list.items(), key=lambda x: last_match[x[0]], reverse=True)}
+def verse() -> None:
     while True:
-        player1 = search_players("Player 1: ", player_list)
-        player2 = search_players("Player 2: ", player_list)
+        player1 = search_players("Player 1: ")
+        player2 = search_players("Player 2: ")
         p = predict(player1[0], player2[0])
         print(f"{player1[1]} vs {player2[1]}: {p} (1:{1/p})")
 
